8f6ebca02f521204d1e7f506a5419dd9afc77a84
perlamarte2003 <43577229+perlamarte2003@users.noreply.github.com>
2019-03-05 09:34:37 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$("button").click(function(){
+   var username= $(".name").val();
+   var hours= $(".years").val();
+   var edad= $(".future").val();
+   
+      var fortune = "Hello " + username + ".";
+     $("p").text(fortune);
+ 
+ 
+
+
+});
+   
+
+   
+ 
